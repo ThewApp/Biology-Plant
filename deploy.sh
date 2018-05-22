6b699a200f75dcfc386814ae629072367b348ba4
Thew <26767607+TheBear44@users.noreply.github.com>
2018-05-22 12:41:58 +0700

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd build
+
+email=$(git config user.email)
+name=$(git config user.name)
+url=$(git remote get-url origin)
+
+git init
+git config user.email $email
+git config user.name $name
+git remote add origin $url
+git checkout -b gh-pages
+git add .
+git commit -m "Deploy gh-pages"
+git push -f origin gh-pages
