058ec0f6869090f268e4f4100c23485938e2abad
Thew <26767607+TheBear44@users.noreply.github.com>
2018-05-27 21:51:10 +0700

    Add CircularProgress while image is loading.

--- src/pages/Overview/Image.js
+++ src/pages/Overview/Image.js
@@ -1,8 +1,10 @@
 import React, { PureComponent } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const styles = theme => ({
   root: {
+    position: "relative",
     display: "block",
     maxWidth: "300px",
     height: 0,
@@ -22,7 +24,7 @@ const styles = theme => ({
       transition: "opacity .5s",
       willChange: "opacity",
       ".expand &.activeImg": {
-        height: "inherit",
+        height: "inherit"
       }
     },
     [theme.breakpoints.up("md")]: {
@@ -32,19 +34,57 @@ const styles = theme => ({
       maxWidth: "600px",
       right: "2vw"
     }
+  },
+  img: {
+    width: "100%",
+    height: "100%"
+  },
+  loading: {
+    "& $img": {
+      opacity: 0
+    }
+  },
+  progress: {
+    [theme.breakpoints.only("xs")]: {
+      color: theme.palette.common.white
+    },
+    position: "absolute",
+    top: "100px",
+    left: "45%"
   }
 });
 
 class Image extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false
+    };
+
+    this.onLoadHandler = this.onLoadHandler.bind(this);
+  }
+
+  onLoadHandler() {
+    this.setState({ loaded: true });
+  }
+
   render() {
     return (
-      <img
+      <div
         className={
-          this.props.classes.root + (this.props.active ? " activeImg" : "")
+          this.props.classes.root +
+          (this.props.active ? " activeImg" : "") +
+          (this.state.loaded ? "" : ` ${this.props.classes.loading}`)
         }
-        src={this.props.src}
-        alt={this.props.alt}
-      />
+      >
+        {this.state.loaded || <CircularProgress className={this.props.classes.progress} />}
+        <img
+          className={this.props.classes.img}
+          src={this.props.src}
+          alt={this.props.alt}
+          onLoad={this.onLoadHandler}
+        />
+      </div>
     );
   }
 }
