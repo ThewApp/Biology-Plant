e0967c001b9023530a593fbb1135f1fbaff57da9
Thew <26767607+TheBear44@users.noreply.github.com>
2018-05-29 20:47:59 +0700

    Move stemModified.md

--- src/pages/Stem/StemModified.js
+++ src/pages/Stem/StemModified.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Markdown from "../../components/Markdown";
 
-import modifiedStemMarkdown from "./markdown/stemModified.md";
+import modifiedStemMarkdown from "./stemModified.md";
 import stemModifiedCreeping from "../../images/stemModifiedCreeping.jpg";
 import stemModifiedTwining from "../../images/stemModifiedTwining.png";
 import stemModifiedTendril from "../../images/stemModifiedTendril.png";
