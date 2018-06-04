260eb9ec7a6079ecc87cbbff6d7ba2aa1dffd0d1
Thew <26767607+TheBear44@users.noreply.github.com>
2018-06-04 20:07:15 +0700

    Update Tissue Collenchyma

--- src/pages/Tissue/Collenchyma.js
+++ src/pages/Tissue/Collenchyma.js
@@ -1,10 +1,40 @@
+import tissueCollenchyma from "../../images/tissueCollenchyma.png";
+import tissueCollenchymaVariant from "../../images/tissueCollenchymaVariant.jpg";
+
 const collenchymaMarkdown = `
 # Collenchyma
+
+ประกอบด้วยเซลล์คอลเลนไคมา (Collenchyma cell) มีลักษณะคล้ายกับเซลล์ [Parenchyma](#/tissue/Parenchyma) คือมีชีวิตแต่ Primary cell wall มีความหนาบางไม่สม่ำเสมอทั่วทั้งเซลล์
+
+![Collenchyma](tissueCollenchyma)
+
+มีหลายรูปแบบ เช่น
+
+* หนารอบๆ เซลล์ เรียกว่า Larmella collenchyma
+
+* หนาตามมุมของเซลล์มากกว่าบริเวณอื่นๆ เรียกว่า Angular collenchyma
+
+* หนาตามช่องว่างระหว่างเซลล์ เรียกว่า Larcunar collenchyma
+
+![Lamella, angular and larcunar collenchyma](tissueCollenchymaVariant)
+
+เซลล์คอลเลนไคมาสามารถยืดตัวได้เมื่อพืชมีการเจริญเติบโตปฐมภูมิ หรือขยายขนาด
+
+## บริเวณที่พบ
+
+ปกติไม่พบในราก แต่มักพบในลำต้น ก้านใบและแผ่นใบ มักพบอยู่เป็นกลุ่มหรือเป็นมัดถัดเข้ามาจากชั้นเนื้อเยื่อผิว
+
+## หน้าที่ Collenchyma
+
+ให้ความแข็งแรงแก่ส่วนที่ยังอ่อนของพืช
 `;
 
 const collenchymaData = {
   markdown: collenchymaMarkdown,
-  images: {}
+  images: {
+    tissueCollenchyma,
+    tissueCollenchymaVariant
+  }
 };
 
 export default collenchymaData;
