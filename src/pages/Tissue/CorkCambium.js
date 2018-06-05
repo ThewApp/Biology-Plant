d959d0c5ffbd5e2a0596d744a157db1e221174ae
Thew <26767607+TheBear44@users.noreply.github.com>
2018-06-05 16:18:04 +0700

    Update Tissue CorkCambium

--- src/pages/Tissue/CorkCambium.js
+++ src/pages/Tissue/CorkCambium.js
@@ -1,10 +1,34 @@
+import tissueCork from "../../images/tissueCork.png";
+import tissueCorkCambiumRoot from "../../images/tissueCorkCambiumRoot.png";
+
 const corkCambiumMarkdown = `
-# Cork cambium / Phellogen
+# Cork cambium / Phellogen (เนื้อเยื่อป้องกัน)
+
+เป็นเนื้อเยื่อเจริญทางด้านข้าง โดยแบ่งเซลล์ออกไปด้านนอกเจริญไปเป็น Cork cell หรือ Phellem เพื่อสร้างเนื้อเยื่อ [Cork](#/tissue/cork) และแบ่งเซลล์เข้าด้านในเป็นเนื้อเยื่อ Phelloderm ซึ่งประกอบด้วยเซลล์พาเรงคิมา ([Parenchyma](#/tissue/parenchyma)) เป็นการเจริญเติบโตขั้นที่สอง
+
+![Cork, cork cambium and phelloderm](tissueCork)
+
+ชั้นเซลล์ที่รวมตั้งแต่ [Cork](#/tissue/cork) ชั้นนอกสุดจนถึงชั้น Phelloderm เรียกว่า **Periderm** เป็นองค์ประกอบส่วนหนึ่งของเปลือก
+
+ในรากพืชใบเลี้ยงคู่เนื้อเยื่อบริเวณเพอริไซเคิล ([Pericycle](#/tissue/pericycle)) เปลี่ยนสภาพมาเป็นคอร์กแคมเบียม (Cork cambium) แล้วสร้างเนื้อเยื่อคอร์กแทนเนื้อเยื่อผิวเดิม
+
+![Cork cambium from pericycle](tissueCorkCambiumRoot)
+
+## บริเวณที่พบ
+
+ใต้ผิวชั้นนอกของรากและลำต้นพืชที่มี Secondary Growth
+
+## หน้าที่ Cork cambium
+
+สร้างส่วนหนึ่งของเปลือกไม้ แบ่งเซลล์เพื่อสร้างเนื้อเยื่อ ([Cork](#/tissue/cork) หรือ Phellem ) ให้มาทำหน้าที่ป้องกันอันตรายและความเสียหายแทนเนื้อเยื่อผิวนอก ([Epidermis](#/tissue/epidermis)) ที่ใกล้หมดอายุ
 `;
 
 const corkCambiumData = {
   markdown: corkCambiumMarkdown,
-  images: {}
+  images: {
+    tissueCork,
+    tissueCorkCambiumRoot
+  }
 };
 
 export default corkCambiumData;
